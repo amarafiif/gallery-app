e2c8b1724db59ad38bbd1a309d16ced1dddb2ca9
Muhammad Ammar Afif <amarafif230@gmail.com>
2023-11-03 19:30:17 +0700

    Inital commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    cors = require('cors'),
+    router = require('./routes/index'),
+    swaggerUi = require('swagger-ui-express'),
+    swaggerJson = require('./openapi.json')
+
+require('dotenv').config()
+
+app.use(express.json({ strict:false }))
+app.use(cors())
+app.use('/images', express.static('public/images'))
+app.use('/api/v1', router)
+
+app.get('*', (req, res) => {
+    return res.status(404).json({
+        error: 'Endpoint is not registered!'
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is running at localhost:${port}`)
+})
